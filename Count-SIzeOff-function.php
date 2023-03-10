0ba7728fd04967c7381edf6540d01eb2b74969fd
Error-404 <119523240+Abuzar-061@users.noreply.github.com>
2023-03-10 10:30:16 +0500

    Add files via upload

---
+++ Count-SIzeOff-function.php
@@ -0,0 +1,18 @@
+<?php
+//Count()
+
+$fruits = array("apple", "banana", "orange", "kiwi");
+// Count function show the $fruits array Size
+$num_fruits = count($fruits);
+echo "There are " . $num_fruits . " fruits in the array.";
+
+echo "<br>";
+
+//Size Off() Do same as count function
+$food = array("apple", "banana", "orange", "kiwi");
+// Count function show the $fruits array Size
+$fruit = count($food);
+echo "There are " . $fruit . " fruits in the array.";
+
+
+?>
